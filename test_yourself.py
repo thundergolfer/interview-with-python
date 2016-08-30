5c4263cefd4147a1b4094fefba3b54fe520be116
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2016-08-30 14:48:57 +1000

    Adding the test_yourself.py for later development

---
+++ test_yourself.py
@@ -0,0 +1,10 @@
+#
+# Author: Jonathon Belotti <thundergolfer>
+#
+# This script can either:
+# 1. Quick run through problems in the repo, asking you if you are confident in that area or not.
+#    It builds a list of problems to brush up on and returns them to user
+#
+# 2. Be more interactive. Selecting an opening up exercises for users and then testing their solutions for them. [This would be non-trivial work]
+
+# TODO: Implement the simple (1) functionality
