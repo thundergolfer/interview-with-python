341f0eb6b6b5be54f8ad0ba88cf8965f6c1bf522
mgbox <mattbox87@gmail.com>
2016-09-03 19:19:15 +1000

    Implementation and tests for two code exercises.
    * added unittesting, moved to master
    
    * base ten to binary

--- solutions/python/all-unique-characters.py
+++ solutions/python/all-unique-characters.py
@@ -1,5 +1,6 @@
 # Check if a string is composed of all unique characters
 # author: Matt Box
+import unittest
 
 def all_unique(string):
     letters = sorted(string)
@@ -7,4 +8,12 @@ def all_unique(string):
         if letters[i] == letters[i + 1]:
             return False
     return True
+
+class Test(unittest.TestCase):
+    def test_sanity(self):
+        self.assertFalse(all_unique("foo"))
+        self.assertTrue(all_unique("bar"))
+
+if __name__ == '__main__':
+    unittest.main()
 
---
+++ solutions/python/base-ten-to-binary.py
@@ -0,0 +1,29 @@
+def base_ten_to_binary(num):
+    # base_ten_to_binary: takes an int, returns the binary representation as a string.
+    
+    # we'll push the digits into this list, least significant first    
+    digits = []
+    while num:
+        if num & 1:
+            digits.append('1')
+        else:
+            digits.append('0')
+        num = num >> 1
+    # if we got here with an empty list num was 0 to begin with
+    if digits == []:
+        digits = ['0']
+
+    digits.reverse()
+    return ''.join(digits)
+
+import unittest
+
+class Test(unittest.TestCase):
+    def test_sanity(self):
+        self.assertEqual(base_ten_to_binary(0), '0')
+        self.assertEqual(base_ten_to_binary(1), '1')
+        self.assertEqual(base_ten_to_binary(10), '1010')
+
+if __name__ == '__main__':
+    unittest.main()
+
