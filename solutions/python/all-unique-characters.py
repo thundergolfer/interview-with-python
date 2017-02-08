c6ac7d444609976feb99bb23752df26678a1b963
Cameron Yick <cameron.yick@yale.edu>
2017-02-08 18:50:45 -0500

    Optimize all-unique-characters.py (#5)
    Was thinking about this problem recently, so added a small constraint that avoids checking strings that must have duplicates by virtue of the fact that they are too long.
    
    Alternately, if it would be best to code up a separate solution in a different file, I could do that too.

--- solutions/python/all-unique-characters.py
+++ solutions/python/all-unique-characters.py
@@ -3,8 +3,14 @@
 import unittest
 
 def all_unique(string):
+    
+    # Optimization that assumes only 255 ASCII characters are possible (ignores possibility of unicode)
+    nLetters = len(letters)
+    if (nLetters > 255):
+        return False
+    
     letters = sorted(string)
-    for i in range(len(letters) - 1):
+    for i in range(nLetters - 1):
         if letters[i] == letters[i + 1]:
             return False
     return True
